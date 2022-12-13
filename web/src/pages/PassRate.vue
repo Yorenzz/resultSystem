e2d45684a030fa8e895f5cacb6c1feba538a1635
YangEzzz <yez0807@163.com>
2022-12-14 06:02:49 +0800

    缓存

--- web/src/pages/PassRate.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-</script>
-
-<template>
-  passrate
-</template>
-
-<style scoped>
-</style>
--- web/src/utils/util.js
+++
@@ -1,7 +0,0 @@
-import storage from './storage.js'
-
-import { TOKEN_KEY } from '../config'
-
-export const getToken = () => {
-	return storage.getItem(TOKEN_KEY)
-}

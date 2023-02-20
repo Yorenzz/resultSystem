e61ef9e271cf7e45e67afe021f57d4e0dc2b2950
YangEzzz <yez0807@163.com>
2023-02-21 00:10:00 +0800

    统计、分数段完成

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,15 +1,19 @@
 const router = require('koa-router')()
 
-router.get('/', async (ctx, next) => {
-	await ctx.render('index', { title: 'Hello Koa 2!' })
-})
+const { Configuration, OpenAIApi } = require('openai')
+const configuration = new Configuration({ apiKey: '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' })
 
-router.get('/string', async (ctx, next) => {
-	ctx.body = 'koa2 string'
+router.post('/chat', async (ctx, next) => {
+	const { question } = ctx.request.body
+	const openai = new OpenAIApi(configuration)
+	const response = await openai.createCompletion({
+		model: 'text-davinci-003',
+		prompt: `${question}`,
+		max_tokens: 2048,
+		temperature: 0.7,
+		stop: [' Human:', ' AI:'],
+	})
+	const res = response.data
+	ctx.body = res
 })
-
-router.get('/json', async (ctx, next) => {
-	ctx.body = { title: 'koa2 json' }
-})
-
 module.exports = router
